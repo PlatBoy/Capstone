55c3aff7017d68a4c8ff99231ec122e650d7158d
PlatBoy <panman231ar@gmail.com>
2026-06-09 01:19:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -36,5 +38,5 @@ def read_root():
     return {"status": "online"}
 
 if __name__ == "__main__":
-port = int(os.environ.get("PORT", 8000))
-uvicorn.run("main:app", host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
