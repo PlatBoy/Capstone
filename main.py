d538299edd5f5540d3de1be2778bd2fee7fa70d6
PlatBoy <panman231ar@gmail.com>
2026-06-09 01:17:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ async def login(request: LoginRequest):
 def read_root():
     return {"status": "online"}
 
-        
 if __name__ == "__main__":
 port = int(os.environ.get("PORT", 8000))
 uvicorn.run("main:app", host="0.0.0.0", port=port)
